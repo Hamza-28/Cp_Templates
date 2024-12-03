9ff24f740aab47f73fd88f19f167ed8b9fd688c1
Md. Saimum Islam Hamza <129812963+Hamza-28@users.noreply.github.com>
2024-12-03 12:30:45 +0600

    Update coordinatePoint.cpp

--- coordinatePoint.cpp
+++ coordinatePoint.cpp
@@ -24,7 +24,23 @@ struct Point {
 	long long operator *(const Point& p) const {
 		return 1LL * x * p.y - 1LL * y * p.x;
 	}
-	friend istream& operator >>(istream& cin, Point& p) {
+	bool isInside(Point& a, Point& b) const { 	// if p is inside segment a-b
+		if ((a - *this) * (b - *this) != 0) return false;
+		bool d1 = this->x >= min(a.x, b.x) and this->x <= max(a.x, b.x); 
+		bool d2 = this->y >= min(a.y, b.y) and this->y <= max(a.y, b.y);
+		return d1 and d2;
+	}
+	bool rayIntersect(Point a, Point b) { 		// if p-q ray intersects segment a-b
+		Point q(this->x, INT32_MAX);		
+		for (int rep = 0; rep < 2; ++rep) {
+			if ((a - *this) * (q - *this) <= 0 and (b - *this) * (q - *this) > 0 and (a - *this) * (b - *this) < 0) {
+				return true;
+			}
+			swap(a, b);
+		}
+		return false;
+	}
+	friend istream& operator >>(istream& cin, Point& p) {  
 		cin >> p.x >> p.y;
 		return cin;
 	}	
