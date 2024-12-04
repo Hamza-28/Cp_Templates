33a2cd07f18bd134ca84d116d26c71a9d575144a
Md. Saimum Islam Hamza <129812963+Hamza-28@users.noreply.github.com>
2024-12-05 03:18:22 +0600

    Update string_hashing.cpp

--- string_hashing.cpp
+++ string_hashing.cpp
@@ -8,9 +8,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int p1 = 137, mod1 = 127657753, p2 = 277, mod2 = 987654319;
+const int p1 = 137, mod1 = 127657753, p2 = 277, mod2 = 987654319; // 911382323, 972663749
 const int N = 1e6 + 3;
-array<int, 2> pref[N], pref2[N];
+array<int, 2> pref[N], rev[N];
 int pw1[N], pw2[N], ipw1[N], ipw2[N];
 
 int power(int a, int n, int mod) {
@@ -35,33 +35,18 @@ void prec() {
   }
 }
 
-array<int, 2> str_hash(string& s) {
-  int n = s.size();
-  int hs1 = 0;
-  for (int i = 0; i < n; ++i) {
-    hs1 = (hs1 + (1LL * s[i] * pw1[i]) % mod1) % mod1;
-  }
-  int hs2 = 0;
-  for (int i = 0; i < n; ++i) {
-    hs2 = (hs2 + (1LL * s[i] * pw2[i]) % mod2) % mod2;
-  }
-  return {hs1, hs2};
-}
-
-void build(string& s, string& s2) {
+void build(string& s) {
   int n = s.size();
   for (int i = 0; i < n; ++i) {
     pref[i][0] = 1LL * s[i] * pw1[i] % mod1;
     if (i) pref[i][0] = (pref[i][0] + pref[i - 1][0]) % mod1;
     pref[i][1] = 1LL * s[i] * pw2[i] % mod2;
     if (i) pref[i][1] = (pref[i][1] + pref[i - 1][1]) % mod2;
-  }
-  int m = s2.size();
-  for (int i = 0; i < m; ++i) {
-    pref2[i][0] = 1LL * s2[i] * pw1[i] % mod1;
-    if (i) pref2[i][0] = (pref2[i][0] + pref2[i - 1][0]) % mod1;
-    pref2[i][1] = 1LL * s2[i] * pw2[i] % mod2;
-    if (i) pref2[i][1] = (pref2[i][1] + pref2[i - 1][1]) % mod2;
+
+    rev[i][0] = 1LL * s[i] * ipw1[i] % mod1;
+    if (i) rev[i][0] = (rev[i][0] + rev[i - 1][0]) % mod1;
+    rev[i][1] = 1LL * s[i] * ipw2[i] % mod2;
+    if (i) rev[i][1] = (rev[i][1] + rev[i - 1][1]) % mod2;
   }
 }
 
@@ -69,23 +54,33 @@ array<int, 2> get_hash(int i, int j) {
   array<int, 2> ans = {0, 0};
   ans[0] = pref[j][0];
   if (i) ans[0] = (pref[j][0] - pref[i - 1][0] + mod1) % mod1;
-  ans[0] = 1LL * ans[0] * ipw1[i] % mod1;
-
   ans[1] = pref[j][1];
   if (i) ans[1] = (pref[j][1] - pref[i - 1][1] + mod2) % mod2;
+  ans[0] = 1LL * ans[0] * ipw1[i] % mod1;
   ans[1] = 1LL * ans[1] * ipw2[i] % mod2;
   return ans;
 }
- 
+
+array<int, 2> get_rev_hash(int i, int j) {
+  array<int, 2> ans = {0, 0};
+  ans[0] = rev[j][0];
+  if (i) ans[0] = (rev[j][0] - rev[i - 1][0] + mod1) % mod1;
+  ans[1] = rev[j][1];
+  if (i) ans[1] = (rev[j][1] - rev[i - 1][1] + mod2) % mod2;
+  ans[0] = 1LL * ans[0] * pw1[j] % mod1;
+  ans[1] = 1LL * ans[1] * pw2[j] % mod2;
+  return ans;
+}
+
 int main() {
   ios_base :: sync_with_stdio(0);
   cin.tie(0);
   
   prec();
 
-  string s1, s2;
-  cin >> s1 >> s2;
-  build(s1, s2);
+  string s;
+  cin >> s;
+  build(s);
 
 
   return 0;
